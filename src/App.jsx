be3ed6cc0a5f42bd85c20ae851059e6e31417ebc
Fariz Aulia Pradipta <farizzed@U4-BDK6BS3.localdomain>
2023-07-21 13:56:55 +0700

    refactor: fix the img src folder

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="container">
       <article>
-        <img src="images/group.svg" alt="StarGazers"/>
+        <img src="../public/images/group.svg" alt="StarGazers"/>
         <hgroup>
           <Welcome name={groupName} />
           <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
