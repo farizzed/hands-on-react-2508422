4ffd4d500d38151bdf8ee4175fbb7422a2211726
Fariz Aulia Pradipta <farizzed@U4-BDK6BS3.localdomain>
2023-07-21 13:06:14 +0700

    feat: add stylings, images, and onclick button with alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 function App() {
+  const groupName = 'StarGazers';
+  const groupNameStyle = {color:"steelblue"}
+  const onClickMe = () => {
+    alert('Hi there!');
+  }
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+      <article>
+        <img src="images/group.svg" alt="StarGazers"/>
+        <hgroup>
+            <h1>Meet the <i style={groupNameStyle}>{groupName}</i></h1>
+            <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+        </hgroup>
+        <button className="outline" onClick={onClickMe}>Click me</button>
+      </article>
+    </div>
   )
 }
 export default App
