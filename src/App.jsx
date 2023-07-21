81bc5fcc5b4b0abf084b9aecb37c000ca9513945
Fariz Aulia Pradipta <farizzed@U4-BDK6BS3.localdomain>
2023-07-21 13:30:33 +0700

    feat: add new component, with React Component and React Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import React from "react";
+
+/* Component class with React Component */
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>Meet the {this.props.name}</h1>
+  }
+}
+
+/* Component with React Hooks */
+const Hello = (props) => {
+  const colorSteelBlue = {color:"steelblue"}
+  return <h1>Meet the <i style={colorSteelBlue}>{props.name}</i></h1>
+}
+
 function App() {
   const groupName = 'StarGazers';
-  const groupNameStyle = {color:"steelblue"}
   const onClickMe = () => {
     alert('Hi there!');
   }
@@ -9,8 +27,9 @@ function App() {
       <article>
         <img src="images/group.svg" alt="StarGazers"/>
         <hgroup>
-            <h1>Meet the <i style={groupNameStyle}>{groupName}</i></h1>
-            <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <Welcome name={groupName} />
+          <Hello name="Aliens" />
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
         </hgroup>
         <button className="outline" onClick={onClickMe}>Click me</button>
       </article>
